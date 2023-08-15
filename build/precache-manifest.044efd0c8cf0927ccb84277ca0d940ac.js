61a5df84f392f569db22129c77428e690938c5d4
samani siewe darix <samanidarix@gmail.com>
2023-08-15 19:00:17 +0100

    add build

---
+++ build/precache-manifest.044efd0c8cf0927ccb84277ca0d940ac.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "ec2fee383da8cb4b819c",
+    "url": "/static/css/main.357742ca.chunk.css"
+  },
+  {
+    "revision": "ec2fee383da8cb4b819c",
+    "url": "/static/js/main.ec2fee38.chunk.js"
+  },
+  {
+    "revision": "117ae24ba6ab772f6e2b",
+    "url": "/static/js/1.117ae24b.chunk.js"
+  },
+  {
+    "revision": "d400d5c4f3e1a79f61c4",
+    "url": "/static/css/2.e0932274.chunk.css"
+  },
+  {
+    "revision": "d400d5c4f3e1a79f61c4",
+    "url": "/static/js/2.d400d5c4.chunk.js"
+  },
+  {
+    "revision": "d8ce31b2bf89b2e152a3",
+    "url": "/static/js/runtime~main.d8ce31b2.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "0c7510befb4e384dcac28bcff952dd88",
+    "url": "/index.html"
+  }
+];
